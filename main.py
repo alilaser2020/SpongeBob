83eae82f250c5afb5fdb52aaa1e94ea8b6bba556
alilaser <alilaser80802gmail.com>
2024-01-13 00:12:51 +0330

    Add version2: background

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 import pgzrun
+from pgzero.actor import Actor
+
+
+def draw():
+    """
+    A method for drawing anything with any change (execute by pgzrun.go())
+    :return:
+    """
+    back.draw()
+
+
+def update():
+    """
+    A method for updating and refresh anything 60 times per second (execute by pgzrun.go())
+    :return:
+    """
+    ...
+
 
 WIDTH = 1280
 HEIGHT = 720
 TITLE = "SpongeBob"
+
+# Define background
+back = Actor("back")
 
 pgzrun.go()
