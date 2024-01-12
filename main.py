d955883f30932756dc5f6ed5abe4f9ccc077c88d
alilaser <alilaser80802gmail.com>
2024-01-13 00:25:51 +0330

    Add version6: correct location

--- main.py
+++ main.py
@@ -3,6 +3,22 @@ from pgzero.actor import Actor
 from pgzero.keyboard import keyboard
 
 
+def actor_correct_location(actor):
+    """
+    A method for determine correction location for actors in game's page (execute by pgzrun.go())
+    :param actor:
+    :return:
+    """
+    if actor.x < -actor.width // 2:
+        actor.x = WIDTH + actor.width // 2
+    if actor.x > WIDTH + actor.width // 2:
+        actor.x = -actor.width // 2
+    if actor.y < -actor.height // 2:
+        actor.y = HEIGHT + actor.height // 2
+    if actor.y > HEIGHT + actor.height // 2:
+        actor.y = -actor.height // 2
+
+
 def draw():
     """
     A method for drawing anything with any change (execute by pgzrun.go())
@@ -21,23 +37,30 @@ def update():
     """
     # Bob section
     if keyboard.right:
-        bob.x += bob.speed
+        bob.x += 5
     if keyboard.left:
-        bob.x -= bob.speed
+        bob.x -= 5
     if keyboard.up:
-        bob.y -= bob.speed
+        bob.y -= 5
     if keyboard.down:
-        bob.y += bob.speed
+        bob.y += 5
+
+    actor_correct_location(bob)
 
     # Patrick section
     if keyboard.d:
-        patrick.x += patrick.speed
+        patrick.x += 5
     if keyboard.a:
-        patrick.x -= patrick.speed
+        patrick.x -= 5
     if keyboard.w:
-        patrick.y -= patrick.speed
+        patrick.y -= 5
     if keyboard.s:
-        patrick.y += patrick.speed
+        patrick.y += 5
+
+    actor_correct_location(patrick)
+
+    # Define Plankton
+    actor_correct_location(plankton)
 
 
 WIDTH = 1280
@@ -51,17 +74,15 @@ back = Actor("back")
 bob = Actor("bob_right_prev_ui")
 bob.x = 300
 bob.y = 550
-bob.speed = 5
 
 # Define patrick
 patrick = Actor("patric_left_prev_ui")
 patrick.x = 950
 patrick.y = 450
-patrick.speed = 5
 
 # Define plankton
 plankton = Actor("plankton_right")
-plankton.x = WIDTH//2
+plankton.x = WIDTH // 2
 plankton.y = HEIGHT * 0.7
 
 pgzrun.go()
